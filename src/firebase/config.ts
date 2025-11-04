4f88326b8ab5777f43b5e48112a4b0a7107bfa33
Leo Vercel <leovercel1@gmail.com>
2025-11-04 20:28:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1296885963-e59cf",
+  "appId": "1:247222472030:web:46af2f38f11ee0988b93cf",
+  "apiKey": "AIzaSyBvqKNqtmZSygy8tjo0Fi7-QscIP_nzSB4",
+  "authDomain": "studio-1296885963-e59cf.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "247222472030"
+};
