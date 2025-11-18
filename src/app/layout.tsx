5d09537af82fac03b58b0c3401814fd5c1921194
Leo Vercel <leovercel1@gmail.com>
2025-11-18 20:15:56 +0000

    apple capable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
     <html lang="pt-BR" suppressHydrationWarning>
        <head>
         <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
         <link rel="apple-touch-icon" href="/images/apple-touch-icon.png" />
       </head>
       <body
